5605cdb7249f5e6e2df34caf549da53916d3dc75
Chun Xie <shomaru@gmail.com>
2013-06-29 23:24:31 +0100

    Add joda-time dependency

--- src/main/java/nothing.java
+++ src/main/java/nothing.java
@@ -1,3 +1,5 @@
+import org.joda.time.DateTime;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Administrator
@@ -6,4 +8,10 @@
  * To change this template use File | Settings | File Templates.
  */
 public class nothing {
+
+    public static void main(String[] args) {
+        DateTime dt = new DateTime(2005, 3, 26, 12, 0, 0, 0);
+        System.out.println(dt.getMonthOfYear());
+        System.out.println(dt.plusMonths(3).getMonthOfYear());
+    }
 }
