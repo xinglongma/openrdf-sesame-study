4a077ab0caeec6417225c79dc33fd2ac828785d7
xinglongma <xinglong.ma@hotmail.co.uk>
2013-06-29 23:45:50 +0100

    add junit4.11 to test jodatime

---
+++ src/test/java/JodaTimeTest.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13-6-29
+ * Time: 下午11:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class JodaTimeTest {
+
+}
