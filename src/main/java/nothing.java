6a05e95e43db255f6265f768a19ede8eaaecdcb1
xinglongma <xinglong.ma@hotmail.co.uk>
2013-06-29 22:21:18 +0100

    test, because pom didn't uploaded

---
+++ src/main/java/nothing.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13-6-29
+ * Time: 下午10:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class nothing {
+}
