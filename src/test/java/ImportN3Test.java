ac455d7d498dc5b732257cf4b6ba4506688a60b2
xinglongma <xinglong.ma@hotmail.co.uk>
2013-07-04 18:33:49 +0100

    add sample. n3 file under test/resource/data

---
+++ src/test/java/ImportN3Test.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xma
+ * Date: 04/07/13
+ * Time: 18:07
+ * To change this template use File | Settings | File Templates.
+ */
+public class ImportN3Test {
+    @Test
+}
