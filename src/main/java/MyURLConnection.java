a514fb45a6f6ddfb21d3562147b35f0bd3c647c0
xinglongma <xinglong.ma@hotmail.co.uk>
2013-08-01 15:59:59 +0100

    add a class for sending HTTP request via SPARQL endpoint

---
+++ src/main/java/MyURLConnection.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.net.*;
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xma
+ * Date: 01/08/13
+ * Time: 13:44
+ * To change this template use File | Settings | File Templates.
+ */
+public class MyURLConnection {
+    public static void main (String [] args)throws Exception{
+        String query="PREFIX id: <http://dblp.rkbexplorer.com/id/> PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX akt: <http://www.aktors.org/ontology/portal#> PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX akt: <http://www.aktors.org/ontology/portal#> PREFIX akts: <http://www.aktors.org/ontology/support#> SELECT * WHERE { ?s rdfs:label ?o } LIMIT 10";
+        query=URLEncoder.encode(query, "UTF-8");
+        URL url =new URL("http://dblp.rkbexplorer.com/sparql/?query="+query);
+        URLConnection urlcon = url.openConnection();
+        BufferedReader in=new BufferedReader(new InputStreamReader(urlcon.getInputStream()));
+        String inputLine;
+        while ((inputLine=in.readLine())!= null)
+            System.out.println(inputLine);
+        in.close();
+    }
+}
