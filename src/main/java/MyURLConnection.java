eea44eddbde557b6ab7ddc7771b55e5065c889bc
xinglongma <xinglong.ma@hotmail.co.uk>
2013-08-02 16:38:29 +0100

    more content in MyURLConnection

--- src/main/java/MyURLConnection.java
+++ src/main/java/MyURLConnection.java
@@ -1,3 +1,5 @@
+import org.json.*;
+
 import java.io.*;
 import java.net.*;
 
@@ -10,15 +12,28 @@ import java.net.*;
  * To change this template use File | Settings | File Templates.
  */
 public class MyURLConnection {
+
+
+
     public static void main (String [] args)throws Exception{
         String query="PREFIX id: <http://dblp.rkbexplorer.com/id/> PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> PREFIX akt: <http://www.aktors.org/ontology/portal#> PREFIX owl: <http://www.w3.org/2002/07/owl#> PREFIX akt: <http://www.aktors.org/ontology/portal#> PREFIX akts: <http://www.aktors.org/ontology/support#> SELECT * WHERE { ?s rdfs:label ?o } LIMIT 10";
         query=URLEncoder.encode(query, "UTF-8");
-        URL url =new URL("http://dblp.rkbexplorer.com/sparql/?query="+query);
+        URL url =new URL("http://dblp.rkbexplorer.com/sparql/?query="+query+"&output=xml");
+       // URL url=new URL ("http://data.open.ac.uk/openlearn/ontology/OpenLearnUnit");
         URLConnection urlcon = url.openConnection();
         BufferedReader in=new BufferedReader(new InputStreamReader(urlcon.getInputStream()));
+
         String inputLine;
         while ((inputLine=in.readLine())!= null)
             System.out.println(inputLine);
+
+        StringBuilder builder = new StringBuilder();
+        for (String line = null; (line = in.readLine()) != null;) {
+            builder.append(line).append("&#92n");
+        }
+        //JSONTokener tokener = new JSONTokener(builder.toString());
+       // JSONArray finalResult = new JSONArray(tokener);
+
         in.close();
     }
 }
---
+++ src/test/java/testMyURLConnection.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xma
+ * Date: 02/08/13
+ * Time: 10:38
+ * To change this template use File | Settings | File Templates.
+ */
+public class testMyURLConnection {
+
+
+}
